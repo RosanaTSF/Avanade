751b65c82ff8ea055feefb1a1154ed2d80011433
Rosana <rosanatsf@gmail.com>
2023-12-07 12:40:19 -0300

    Experimentos em C#.

---
+++ Models2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Models2.Models;    
+using System.Globalization;
+
+    try 
+    {
+        string[] linhas = File.ReadAllLines("Binder/Sheets.txt");
+
+        foreach (string linha in linhas)
+        {
+            Console.WriteLine(linha);
+        }
+    } catch(Exception ex)
+    {
+        Console.WriteLine($"A generic exception occurred. {ex.StackTrace}");
+    } finally 
+    {
+        Console.WriteLine("Cigarras - Forfun"); 
+    }
+
+
