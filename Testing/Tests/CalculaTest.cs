06064ac03f66fc4d500a7816c3a96a8102e1f32c
Rosana <rosanatsf@gmail.com>
2023-12-28 13:52:11 -0300

    Experimentos em C# (Comentarios provisórios para estudo).

--- Testing/Tests/CalculaTest.cs
+++ Testing/Tests/CalculaTest.cs
@@ -1,19 +1,18 @@
 using Calculator.Services;
-using Xunit;
-
-namespace Tests
+using Xunit; // Importa biblioteca de testes. O código possui um conjunto de testes unitários para verificar o comportamento da classe Implements em relação às funções Somar e Par, assim Xunit é usado como estrutura de teste.
+namespace Tests // Define a namespace Tests para encapsular as classes de teste.
 {
     public class CalculaTest
     {
-        private readonly Implements _calc;
+        private readonly Implements _calc; // Declara uma instância privada da classe Implements chamada _calc. Essa instância será usada nos métodos de teste.
 
-        public CalculaTest()
+        public CalculaTest() // Declara um construtor para a classe CalculaTest, que inicializa a instância _calc criando uma nova instância da classe Implements.
         {
             _calc = new Implements();
         }
 
-        [Fact]
-        public void DeveSomar2Com4Retornar6()
+        [Fact] // Define métodos de teste usando o Xunit. O método DeveSomar2Com4Retornar6 testa a função Somar da classe Implements. Os métodos seguintes testam a função Par.
+        public void DeveSomar2Com4Retornar6() // Um método de teste de fato ([Fact]) que verifica se a soma de 2 e 4 é igual a 6 usando o método Assert.Equal.
         {
             // Arrange
             int num1 = 2;
@@ -27,7 +26,7 @@ namespace Tests
         }
 
         [Fact]
-        public void DeveVerificarSe10EhParERetornarTrue()
+        public void DeveVerificarSe10EhParERetornarTrue() // Um método de teste de fato ([Fact]) que verifica se o número 10 é par usando o método Assert.True.
         {
             // Arrange
             int num = 10;
@@ -44,14 +43,14 @@ namespace Tests
         [InlineData(4)]
         [InlineData(6)]
         [InlineData(8)]
-        [InlineData(10)]
+        [InlineData(10)] // Um método de teste de teoria ([Theory]) que aceita vários conjuntos de dados (números) para verificar se são pares usando o método Assert.True.
 
         public void DeveVerificarSeNumerosSaoParesRetornarTrue(int num)
         {
             //Act
-             bool result = _calc.Par(num);
+             bool result = _calc.Par(num); // Chama o método Par da instância _calc com o número fornecido no cenário de teste.
             //Assert
-            Assert.True(result);
+            Assert.True(result);  // Afirma que o resultado da chamada do método Par deve ser verdadeiro para o cenário de teste atual.
         }
 
         //Outra forma com arrays:
--- Testing/Tests/CharTest.cs
+++ Testing/Tests/CharTest.cs
@@ -5,29 +5,29 @@ using System.Threading.Tasks;
 using Calculator;
 using Calculator.Services;
 {
-    
+
 }
-
-namespace Tests
+namespace Tests // Define um namespace chamado Tests para encapsular as classes de teste.
 {
-    public class CharTest
+    public class CharTest // Declara uma classe de teste chamada CharTest.
     {
-        private ValidChar _valid;
+        private ValidChar _valid; // Declara uma instância privada da classe ValidChar chamada _valid. Essa instância será usada nos métodos de teste.
 
-        public CharTest()
+        public CharTest()  // Declara um construtor para a classe CharTest, que inicializa a instância _valid criando uma nova instância da classe ValidChar.
         {
-            _valid = new ValidChar();
+            _valid = new ValidChar(); 
         }
+        
         [Fact]
         public void DeveContar3CaracteresEmGodERetornar3()
         {
+        // Um método de teste de fato ([Fact]) que verifica se o método CounterChar da classe ValidChar retorna 3 ao contar os caracteres na string "God".       {
             //Arreng
-            string stories = "God";
+            string stories = "God"; // Declara uma string chamada stories com o valor "God".
             //Act
-            int result = _valid.CounterChar(stories);
+            int result = _valid.CounterChar(stories); // Chama o método CounterChar da instância _valid passando a string stories como argumento e armazena o resultado na variável result.
             //Assert
-            Assert.Equal(3, result);
-
+            Assert.Equal(3, result); // Afirma que o resultado da chamada do método CounterChar deve ser igual a 3 usando o método Assert.Equal.
         }
     }
 }
