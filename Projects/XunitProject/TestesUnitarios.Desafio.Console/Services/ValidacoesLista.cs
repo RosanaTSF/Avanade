f4b6409eb75eae94ad6da16bb3f96e68905dd347
Rosana <rosanatsf@gmail.com>
2024-01-04 11:27:59 -0300

    Comentarios provisórios para estudo.

--- Projects/XunitProject/TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ Projects/XunitProject/TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -5,29 +5,29 @@ namespace TestesUnitarios.Desafio.Console.Services
         public List<int> RemoverNumerosNegativos(List<int> lista)
         {
             var listaSemNegativos = lista.Where(x => x > 0);
-            return listaSemNegativos.ToList();
+            return listaSemNegativos.ToList(); // Entrada: Lista de números inteiros.Saída: Nova lista contendo apenas números positivos.Lógica: Utiliza a função Where para filtrar os números maiores que zero.
         }
 
         public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
         {
             var contem = lista.Contains(numero);
-            return contem;
+            return contem; // Entrada: Lista de números inteiros, número a ser verificado. Saída: Valor booleano indicando se o número está presente na lista.Lógica: Utiliza a função Contains para verificar a presença do número na lista.
         }
 
         public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
         {
             var listaMultiplicada = lista.Select(x => x * numero).ToList();
-            return listaMultiplicada;
+            return listaMultiplicada; // Entrada: Lista de números inteiros, número pelo qual multiplicar. Saída: Lista resultante da multiplicação. Lógica: Utiliza a função Select para multiplicar cada elemento da lista pelo número especificado.
         }
 
         public int RetornarMaiorNumeroLista(List<int> lista)
         {
-            return lista.Max();
+            return lista.Max(); // Entrada: Lista de números inteiros.Saída: Maior número presente na lista. Lógica: Utiliza a função Max para encontrar o valor máximo na lista.
         }
 
         public int RetornarMenorNumeroLista(List<int> lista)
         {
-            return lista.Min();
+            return lista.Min(); // Entrada: Lista de números inteiros. Saída: Menor número presente na lista. Lógica: Utiliza a função Min para encontrar o valor mínimo na lista.
         }
     }
 }
--- Projects/XunitProject/TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ Projects/XunitProject/TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -5,19 +5,19 @@ namespace TestesUnitarios.Desafio.Console.Services
         public int RetornarQuantidadeCaracteres(string texto)
         {
             var numeroCaracteres = texto.Length;
-            return numeroCaracteres;
+            return numeroCaracteres; // Entrada: Uma string. Saída: Um número inteiro representando a quantidade de caracteres na string. Lógica: Utiliza a propriedade Length da string para obter o número de caracteres.
         }
 
         public bool ContemCaractere(string texto, string textoProcurado)
         {
             var contem = texto.Contains(textoProcurado);
-            return contem;
+            return contem; // Entrada: Duas strings, a string principal e a substring a ser procurada. Saída: Valor booleano indicando se a substring está presente na string principal. Lógica: Utiliza a função Contains para verificar se a substring está contida na string principal.
         }
 
         public bool TextoTerminaCom(string texto, string textoProcurado)
         {
             var termina = texto.EndsWith(textoProcurado);
-            return termina;
+            return termina; // Entrada: Duas strings, a string principal e a substring a ser verificada no final da string principal. Saída: Valor booleano indicando se a string principal termina com a substring especificada. Lógica: Utiliza a função EndsWith para verificar se a string principal termina com a substring fornecida.
         }
     }
 }
